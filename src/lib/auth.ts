e9c145984dcf54e606449fc2b151bc5da4c36f02
hashir <hashir704x@gmail.com>
2026-03-24 16:43:09 +0500

    added a sign up listner in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../database/db";
 import * as schema from "../database/schema";
 import { organization } from "better-auth/plugins";
+import { userNotificationSettingTable } from "../database/schema";
 
 if (!process.env.FRONTEND_URL) {
   throw new Error("FRONTEND_URL is not set in the .env file");
@@ -22,4 +23,15 @@ export const auth = betterAuth({
     defaultCookieAttributes: { sameSite: "none", secure: true, httpOnly: true },
   },
   trustedOrigins: [process.env.FRONTEND_URL],
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          await db
+            .insert(userNotificationSettingTable)
+            .values({ userId: user.id });
+        },
+      },
+    },
+  },
 });
